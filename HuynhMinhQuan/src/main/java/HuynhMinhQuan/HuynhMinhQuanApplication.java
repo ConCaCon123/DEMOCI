1f3b3e969f64eb5937ba45ac1c20ef0df9534c04
HuynhMinhQuan <quanmapday123@gmail.com>
2026-03-26 15:54:11 +0700

    gfbs

---
+++ HuynhMinhQuan/src/main/java/HuynhMinhQuan/HuynhMinhQuanApplication.java
@@ -0,0 +1,13 @@
+package HuynhMinhQuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuynhMinhQuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuynhMinhQuanApplication.class, args);
+	}
+
+}
---
+++ HuynhMinhQuan/src/test/java/HuynhMinhQuan/HuynhMinhQuanApplicationTests.java
@@ -0,0 +1,13 @@
+package HuynhMinhQuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuynhMinhQuanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
